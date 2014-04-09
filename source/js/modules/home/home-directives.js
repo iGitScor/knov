52d2fd9ab92662297168c418990f8ed1d4102399
iGitScor <sebastien.correaud@gmail.com>
2014-04-09 09:57:54 +0000

    Tooltip position + HTML Layout

--- source/js/modules/home/home-directives.js
+++ source/js/modules/home/home-directives.js
@@ -2,16 +2,21 @@ define([
     './module',
     'jquery',
     'text!./word.html'
-], function(directives, $, wordTpl) {
+], function(directives, $, linkTpl) {
     'use strict';
 
     directives.directive('knovWord', ['$interval',
         function($interval) {
-            function word(scope, element, attrs) {
+            function link(scope, element, attrs) {
                 var timeoutId;
-                var $word = $(element).find('a');
+                var $link = $(element).find('a');
 
                 function update() {
+                    if ($link.offset().left < ($('body').width() / 2) ) {
+                        $link.parent().addClass('hint--right');
+                    } else {
+                        $link.parent().addClass('hint--left');
+                    }
                 }
 
                 scope.$watch(attrs.myCurrentTime, function(value) {
@@ -21,6 +26,17 @@ define([
                 element.on('$destroy', function() {
                     $interval.cancel(timeoutId);
                 });
+                
+                // Hide link
+                element.on('click', function(e) {
+                    e.preventDefault();
+
+                    if ($link.attr('data-link').length > 0) {
+		                window.open($link.attr('data-link'), '_blank');
+                    }
+                    
+                    return false;
+	            });
 
                 timeoutId = $interval(function() {
                     update();
@@ -29,8 +45,8 @@ define([
             
             return {
                 restrict: 'A',
-                template: wordTpl,
-                word: word
+                template: linkTpl,
+                link: link
             };
         }
     ]);
