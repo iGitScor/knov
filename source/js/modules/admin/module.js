ec3b8aa24dc938b649267d53eaa10f7afe69a8db
Thibaud Colas <thibaudcolas@gmail.com>
2014-04-14 00:50:02 +0200

    Empty gh-pages branch

--- source/js/modules/admin/module.js
+++
@@ -1,8 +0,0 @@
-/**
- * Attach controllers to this module
- **/
-define(['angular', '../../config', '../../services'], function(ng) {
-    'use strict';
-
-    return ng.module('app.admin', ['app.constants', 'app.services']);
-});
